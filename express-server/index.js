8f0e1ebd6517bcab065fe47344d2f48d2604f3dd
Wandersondu <wdutra@outlook.pt>
2021-11-11 22:01:07 +0000

    Curso FullStack

---
+++ express-server/index.js
@@ -0,0 +1,36 @@
+const express = require('express')
+const path = require('path')
+const app = express()
+
+
+// Definindo o template engine
+app.set('view engine', 'ejs')
+
+
+// Definindo os aquivos estáticos
+//app.use(express.static(path.join(__dirname, 'views')))
+
+
+// Definindo os aquivos públicos
+app.use(express.static(path.join(__dirname, 'public')))
+
+
+
+// Rotas
+app.get('/', (req, res) => {
+    res.render('index')
+})
+
+
+
+// 404 error (not found)
+app.use((req, res) => {
+    res.send('Página não encontrada!')
+})
+
+
+
+
+// Executando o servidor
+const port = process.env.PORT || 8080
+app.listen(port, () => console.log(`Server is listening on port ${port}`))

26d18058a0ad67238d980f545071aa1d0362b1ac
Wandersondu <wdutra@outlook.pt>
2022-02-23 23:40:57 +0000

    atualiando arquivos

--- JavaScript/aula02/script.js
+++ JavaScript/aula02/script.js
@@ -1,3 +1,25 @@
-var nome = 'Dutra'
-var idade = 32 
-console.log(nome, idade)
+/*
+== igualdade valores
+=== igualdade valores e tipos
+!= diferença de valores
+!== diferenças valores e tipos
+> maior
+> menor
+*/
+
+
+/*
+let nome = 'Dutra'
+let idade = 32
+let profissao = 'Programador'
+console.log(` Olá, meu nome é ${nome} tenho ${idade} anos e sou ${profissao}.`)
+
+*/
+
+let idade = 19
+
+if(idade >= 18 ) {
+    document.write('maior de idade')
+}else{
+    document.write(' +18')
+}

d1d2df5e28e9517f120566311f3ee7a85fb8262e
Wandersondu <wdutra@outlook.pt>
2022-03-01 23:29:57 +0000

    atualizando curso

---
+++ JavaScript/aula10/script.js
@@ -0,0 +1,9 @@
+let inputNota1 = prompt('Primeira nota: ')
+let inputNota2 = prompt('Segunda nota: ')
+
+let nota1 = parseInt(inputNota1)
+let nota2 = parseInt(inputNota2)
+
+let media = (note1 + nota2) / 2
+
+console.log(`A média  é ${media}! `)

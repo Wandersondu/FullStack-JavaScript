95df41b98fed5a7ee94b16c6e28f011d8922f8c4
Wandersondu <wdutra@outlook.pt>
2022-03-02 23:33:31 +0000

    atualizaçao de arquivos

--- JavaScript/aula10/script.js
+++ JavaScript/aula10/script.js
@@ -4,12 +4,9 @@ let inputNota2 = prompt('Segunda nota: ')
 let nota1 = parseInt(inputNota1)
 let nota2 = parseInt(inputNota2)
 
-let media = (note1 + nota2) / 2
-
 let mediaMinima = 7
 
-let media = (nota1 + nota2) - 2
-
+let media = (nota1 + nota2) / 2
 
 if (media >= mediaMinima) {
     document.write('você esta aprovado!')
@@ -19,5 +16,7 @@ if (media >= mediaMinima) {
 
 
 if (media >= mediaMinima && media === 10) {
-    document.write('Nossa esta parabéns')
+    document.write('Nossa está de parabéns')
+} else if (media >= mediaMinima && media === 9 ) {
+    document.write('mandou bem!')
 }

f68e6e261a902083980f9d319ce9290856354e8c
Wandersondu <wdutra@outlook.pt>
2022-03-04 00:38:17 +0000

    uploard files

---
+++ nodejs/aula01/calculos.js
@@ -0,0 +1,16 @@
+
+function soma(n1, n2) {
+    return n1 + n2
+}
+
+
+function multiplicacao(n1, n2) {
+    return n1 * n2
+}
+
+
+
+module.exports = {
+    soma,
+    multiplicacao
+}
---
+++ nodejs/aula01/index.js
@@ -0,0 +1,5 @@
+const { soma, multiplicacao } = require('./calculos.js')
+
+const resultado = soma(10, 20)
+
+console.log(resultado)

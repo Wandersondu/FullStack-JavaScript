193b05664c3b6122d6508ec7449cbfd97c2810c3
Wandersondu <wdutra@outlook.pt>
2022-04-20 00:00:25 +0100

    adicionando estudos

--- JavaScript/javascript-intermediario/aula01/script.js
+++ JavaScript/javascript-intermediario/aula01/script.js
@@ -1,3 +1,20 @@
 //querySelector - seleciona aoenas um elemento 
 //queryselectoall  - seleciona mais elementos 
 
+let menuMobile = document.querySelector('.menuMobile')
+let botaoMenu = document.querySelector('.botaoMenu')
+
+let aberto = false //flag controlando o abre e fecha45
+function abreFecha(){
+
+    if(aberto === true){
+        aberto = false
+        menuMobile.classList.remove('aberto')
+        botaoMenu.innerText = 'Abrir Menu'
+    } else if(aberto === false){
+        aberto = true
+        menuMobile.classList.add('aberto')
+        botaoMenu.innerText = 'Fechar Menu'
+    }
+
+}

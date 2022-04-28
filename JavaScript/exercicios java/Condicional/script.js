41d71619bc7207f2f767ecd21bf7e6b5de9c8c62
Wandersondu <wdutra@outlook.pt>
2022-04-28 22:58:28 +0100

    estuando condicioanis

--- JavaScript/exercicios java/Condicional/script.js
+++ JavaScript/exercicios java/Condicional/script.js
@@ -1,4 +1,4 @@
-
+/*
 
 let nome = prompt('Qual é o nome do Piloto?')
 alert('A velocidade da Nave é 0 Km')
@@ -8,8 +8,39 @@ if(velocidade <= 0){
     alert('Nave está parada. Iniciar propursores')
 }else if(velocidade < 40 ) {
     alert('Velocidade baixa, Gostaria de almentar?')
-}else if(velocidade >= 40 || velocidade < 80 ) {
+}
+else if(velocidade >= 40 || velocidade < 80 ) {
     alert('Parece uma boa velocidade')
 }else if(velocidade >= 80 || velocidade < 100) {
     alert('Velocidade alta. Considere diminuir')
+}
+
+*/
+
+let piloto = prompt('Qual é o nome do Piloto?')
+
+let velocity = 0
+
+let newVelocity = prompt('A que velocidade vocÊ gostaria de acelerar?')
+let confirmVelocity = confirm(`Estamos acelerando para ${newVelocity} Km/s `)
+
+if(confirmVelocity) {
+    velocity = newVelocity
+}
+
+if(velocity <= 0) {
+    alert('Nave está parada. para partir aumenta a velocidade.')
+} else {
+    if(velocity < 40) {
+        alert('Você está muito devagar. Gostaria de almentar mais.')
+    } else {
+        if(velocity < 80) {
+            alert('A velocidade está otima.')
+        } else {
+            if(velocity < 100 )
+            alert ('velidade alta.')
+        } else {
+            alert('Velocidade perigosa.travões ligados')
+        }
+    }
 }
